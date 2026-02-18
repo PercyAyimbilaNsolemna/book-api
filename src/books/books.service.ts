71cb5054c30695ea868f7cabbd2fd1308a9b78ec
Ayimbila Nsolemna Percy <percy.ayimbila@amalitech.com>
2026-02-18 21:07:31 +0000

    feat: add logic for retrieving a book.

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,11 +1,14 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { Book, BookDocument } from './schemas/books.schema';
 import { CreateBookDto } from './dto/create-book.dto';
 
 @Injectable()
 export class BooksService {
+  findBook(bookID: string): Book | PromiseLike<Book> {
+    throw new Error('Method not implemented.');
+  }
     constructor(@InjectModel(Book.name) private booksModel: Model<BookDocument>) {}
 
     // Create a new book
@@ -31,4 +34,21 @@ export class BooksService {
       .find({ availableQuantity: { $gt: 0 } }) // Only books in stock
       .exec();
   }
+
+  // Get book by ID
+  async findBookById(id: string): Promise<BookDocument> {
+
+  // Validate MongoDB ObjectId
+  if (!Types.ObjectId.isValid(id)) {
+    throw new NotFoundException('Invalid book id');
+  }
+
+  const book = await this.booksModel.findById(id).exec();
+
+  if (!book) {
+    throw new NotFoundException('Book not found');
+  }
+
+  return book;
+}
 }
