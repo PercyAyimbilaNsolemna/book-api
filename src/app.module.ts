09a7892dfba4b3f738a25f9893613a6471be28a0
Ayimbila Nsolemna Percy <percy.ayimbila@amalitech.com>
2026-02-18 20:24:46 +0000

    refactor: add the books controller, service, and module to the app module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [],
+    imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot('mongodb://localhost:27017/bookstore'),
+    BooksModule,
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
